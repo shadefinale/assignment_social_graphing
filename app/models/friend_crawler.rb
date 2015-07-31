2cd279c02ffda3345c6cc0ce069ce37fb64ff698
Nick Sarlo <nicholas.l.sarlo@gmail.com>
2015-07-30 20:26:37 -0400

    friend crawler created

--- app/models/friend_crawler.rb
+++ app/models/friend_crawler.rb
@@ -1,6 +1,26 @@
+Node = Struct.new(:user, :children, :parent, :depth)
+
 class FriendCrawler
+  attr_reader :root
+
   def initialize(user, degrees=3)
     @degrees = degrees
-    @root = user
+    @root = Node.new(user, [], nil, 1)
   end
+
+  def parse
+    queue = [@root]
+    until queue.empty?
+      current = queue.shift
+      break if current.depth > @degrees
+      if current.user.followed_users.any?
+        current.user.followed_users.each do |child|
+          c = Node.new(child, [], current, current.depth + 1)
+          current.children << c
+          queue << c
+        end
+      end
+    end
+  end
+
 end
